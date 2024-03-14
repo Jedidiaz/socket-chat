3c9579ad169af791b6551754efba365da4465d69
Jedidiaz <jedidiazfagundez@gmail.com>
2024-03-14 16:41:15 -0500

    update: file app client

--- socket-client/src/App.jsx
+++ socket-client/src/App.jsx
@@ -1,7 +1,10 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import HomePage from "./views/home";
+import { io } from "socket.io-client";
 
 const App = () => {
+  const IO = io("http://localhost:3002");
+
   return (
     <Router>
       <Routes>
